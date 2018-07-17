eb3a1e2c74b83b7e3357fda592e9944e7544b30d
La Luo <la_1210@icloud.com>
2018-07-16 22:51:59 -0700

    cashed session

--- mysite_django/settings.py
+++ mysite_django/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache" 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
